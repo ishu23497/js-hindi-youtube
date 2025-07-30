b430964860efca6e8c8fa221011f486c7f34ce50
ishu23497 <royalmansingh98@gmail.com>
2025-07-30 01:12:50 +0000

    changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 16626
+let accountEmail = "mansingh98@gmail.com"
+var accountPassword = "15536"
+accountCity = "jaipur"
+
+// accountId = 2 
+accountEmail = "ajju9760@gmail.com"
+accountPassword = "745737"
+accountCity = "agra"
+// preper not to use var 
+
+console.log(accountId);
+console.table([accountEmail,accountId,accountPassword,accountCity])

a3166f88ea2e2dc05117887a0a6b479ea559f39c
ishu23497 <royalmansingh98@gmail.com>
2025-07-30 04:44:51 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,14 @@
+"use strict" //treat all js  code as newer version
+let name = "ishu"
+let age = 18
+let isLooggedIn = false
+
+// number =>2 to power 53
+// bigint 
+// string  => "ishu" ,'ishu'
+// boolean =>true ,false
+// null => standalone value
+console.log(typeof "ishu") // the type of  hme data type bta deta hai ki ye string hai ya int
+console.log(typeof undefined);// undefined
+console.log(typeof null); //object
+
